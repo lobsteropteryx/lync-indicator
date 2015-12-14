5c9fb6b167f75d314f4eca4e3ad7db65246939d6
Ian Firkin <ian.firkin@gisinc.com>
2015-12-13 20:17:16 -0500

    Adds Lync status monitor
    Adds a working windows service, which prints out the availability to
    the console whenever it changes.

---
+++ LyncIndicator/LyncIndicator/LyncPresenceService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LyncIndicator
+{
+    public partial class LyncPresenceService : ServiceBase
+    {
+        private LyncStatusWatcher lyncStatusWatcher;
+
+        public LyncPresenceService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            this.lyncStatusWatcher = new LyncStatusWatcher();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ LyncIndicator/LyncIndicator/LyncStatusWatcher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Lync.Model;
+
+namespace LyncIndicator
+{
+    class LyncStatusWatcher
+    {
+        private LyncClient lyncClient;
+
+        public LyncStatusWatcher()
+        {
+            this.lyncClient = LyncClient.GetClient();
+            lyncClient.Self.Contact.ContactInformationChanged +=
+                new EventHandler<ContactInformationChangedEventArgs>(SelfContact_ContactInformationChanged);
+        }
+
+        #region Lync Event Handlers
+        /// <summary>
+        /// Handler for the ContactInformationChanged event of the contact. Used to update the contact's information in the user interface.
+        /// </summary>
+        private void SelfContact_ContactInformationChanged(object sender, ContactInformationChangedEventArgs e)
+        {
+            //Only update the contact information in the user interface if the client is signed in.
+            //Ignore other states including transitions (e.g. signing in or out).
+            if (lyncClient.State == ClientState.SignedIn)
+            {
+                //Get from Lync only the contact information that changed.
+                if (e.ChangedContactInformation.Contains(ContactInformationType.Availability))
+                {
+                    this.UpdateAvailability();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the contact's current availability value from Lync and updates the corresponding elements in the user interface
+        /// </summary>
+        private void UpdateAvailability()
+        {
+            //Get the current availability value from Lync
+            ContactAvailability currentAvailability = currentAvailability = 
+                (ContactAvailability)lyncClient.Self.Contact.GetContactInformation(ContactInformationType.Availability);
+            Console.WriteLine(currentAvailability.ToString());
+        }
+
+        # endregion
+        
+    }
+}
