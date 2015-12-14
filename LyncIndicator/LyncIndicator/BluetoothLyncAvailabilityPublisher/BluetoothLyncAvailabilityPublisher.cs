aefddf3d980fd55bb6ae696483d166878d67c000
Ian Firkin <ian.firkin@gisinc.com>
2015-12-13 20:31:03 -0500

    Adds scaffolding for bluetooth publisher
    - Adds interface for status publication
    - Adds bluetooth implementation

---
+++ LyncIndicator/LyncIndicator/BluetoothLyncAvailabilityPublisher/BluetoothLyncAvailabilityPublisher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LyncAvailabilityPublisher
+{
+    class BluetoothLyncAvailabilityPublisher : ILyncAvailabilityPublisher
+    {
+        public void Send(string availability)
+        {
+
+        }
+    }
+}
---
+++ LyncIndicator/LyncIndicator/BluetoothLyncAvailabilityPublisher/ILyncAvailabilityPublisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LyncAvailabilityPublisher
+{
+    interface ILyncAvailabilityPublisher
+    {
+        void Send(string availability);
+    }
+}
